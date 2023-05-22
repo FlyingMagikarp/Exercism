1b4f43bd843e9cbc22a2f0dc1db5f9ae61ce9229
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2023-05-22 17:12:13 +0200

    progress

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-use std::char;
+
 
 const PRIMES: [i64; 26] = [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
@@ -8,22 +8,25 @@ const PRIMES: [i64; 26] = [
 
 fn map_string_to_prime_product(input: &str) -> i64 {
     input.chars()
-        .map(char::to_lowercase)
+        //.map(char::to_lowercase)
         .map(|c| (c as u8) - b'a')
         .map(|cb| PRIMES[cb as usize])
-    .product();
-
-
-    return 1;
+        .product()
 }
 
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    let mut result: HashSet<&'a str> = HashSet::new();
+    let mut result: HashSet<&str> = HashSet::new();
 
-    let tmp = PrimeMapper::A.value().to_string();
-    let tmpa = "A";
-    tmp = PrimeMapper::tmpa
+    for &x in possible_anagrams{
+        if &word == &x {
+            continue;
+        }
+
+        if map_string_to_prime_product(&word) == map_string_to_prime_product(x) {
+            result.insert(x);
+        }
+    }
 
     return result;
 }
