25e6edfa9f1c895dbad5e46697d442be0b7dd948
Alvaro Fernandez <alvaro.fernandez@transgourmet.ch>
2023-05-23 10:48:22 +0200

    next

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,32 +1,21 @@
 use std::collections::HashSet;
 
-
-const PRIMES: [i64; 26] = [
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-    73, 79, 83, 89, 97, 101,
-];
-
-fn map_string_to_prime_product(input: &str) -> i64 {
-    input.chars()
-        //.map(char::to_lowercase)
-        .map(|c| (c as u8) - b'a')
-        .map(|cb| PRIMES[cb as usize])
-        .product()
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lower);
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate_lower = candidate.to_lowercase();
+            candidate_lower.len() == word_lower.len()
+                && candidate_lower != word_lower
+                && get_sorted(&candidate_lower) == word_sorted
+        })
+        .copied()
+        .collect()
 }
-
-
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    let mut result: HashSet<&str> = HashSet::new();
-
-    for &x in possible_anagrams{
-        if &word == &x {
-            continue;
-        }
-
-        if map_string_to_prime_product(&word) == map_string_to_prime_product(x) {
-            result.insert(x);
-        }
-    }
-
-    return result;
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
+    word_sorted.sort_unstable();
+    word_sorted
 }
